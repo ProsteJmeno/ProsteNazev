567289553c573199f302b70c1c6ee935ee0b410d
BuildTools <unconfigured@null.spigotmc.org>
2019-08-19 17:47:06 +0200

    ProsteNeco

---
+++ Assets/scripts/genderSceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class genderSceneScript : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public void Man()
+    {
+        PlayerPrefs.SetInt("gender", 1);
+        SceneManager.LoadScene("mngrScene");
+    }
+    public void Woman()
+    {
+        PlayerPrefs.SetInt("gender", 2);
+        SceneManager.LoadScene("mngrScene");
+    }
+    public void Third()
+    {
+        PlayerPrefs.SetInt("gender", 3);
+        SceneManager.LoadScene("mngrScene");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("something");
+    }
+}
---
+++ Assets/scripts/rbmovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rbmovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    Rigidbody2D rb;
+    public Vector2 movement;
+    bool jump;
+    public float JumpForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+        jump = Input.GetKeyDown(KeyCode.Space);
+    }
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+       
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        //insert one of the 3 types of movement here
+        rb.velocity = direction * speed;
+    }
+    void jumpCharacter(bool jump)
+    {
+        if (jump)
+        {
+            rb.AddForce(new Vector2 )
+        }
+    }
+    
+}
